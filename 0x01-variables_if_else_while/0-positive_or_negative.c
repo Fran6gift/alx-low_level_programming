cfdaca5d97dca31b7166df9551bf3248677fffe3
Fran6gift <justinhishie@gmail.com>
2022-09-21 08:49:25 -0700

    identify positie negative and zero numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,9 +10,10 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-/* your code goes there */
+
 	if (n>0)
 	{
 		printf("%d is positive\n", n);
@@ -25,5 +26,6 @@ int main(void)
 	{
 		printf("%d is negative\n", n);
 	}
+
 	return (0);
 }
