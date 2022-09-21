6eef83820cefaff8e6d17f04e001506af4f9c31d
Fran6gift <justinhishie@gmail.com>
2022-09-21 09:52:26 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		alpha += 1;
+	}
+	putchar('\n');
+
+	return (0)
+}
