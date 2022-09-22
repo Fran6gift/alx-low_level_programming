368c5a2ebc771dba39665fa57f0ea777482dcea6
Fran6gift <justinhishie@gmail.com>
2022-09-21 18:08:03 -0700

    printinbase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int w = 48;
+	int al = 'a';
+
+	while (w <= 57)
+	{
+		putchar(w);
+		n += 1;
+	}
+	while (al <= 'f')
+	{
+		putchar(al);
+		al++;
+	}
+	putchar('\n');
+
+	return (0);
+}
