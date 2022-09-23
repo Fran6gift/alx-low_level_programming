884db18501fbb6e6017d681a5a47e1553896516f
Fran6gift <justinhishie@gmail.com>
2022-09-22 17:44:01 -0700

    absolute val of integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  _abs - function that computes the absolute value of an integer.
+ *  r: input to be checked
+ *  Return: absolute value of r
+ */
+int _abs(int r)
+{
+	if (r < 0)
+	{
+		r = r * (-1);
+		return (r);
+	}
+	else
+		return (r);
+}
