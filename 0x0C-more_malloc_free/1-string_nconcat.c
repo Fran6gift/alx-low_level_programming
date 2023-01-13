7586888ba7e0e6772019d68831dded91d391fd55
Fran6gift <111250094+Fran6gift@users.noreply.github.com>
2023-01-13 21:48:35 +0100

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,50 +1,52 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * string_nconcat - len of 1st str, len of 2nd str, if n < 2nd, 2nd = n
- * 2nd + 1st = total len, malloc + null byte, loop to insert into temp arr
- * @s1: input one
- * @s2: input two
- * @n: s2's number of bytes
- * Return: 0
+ * string_nconcat - function that concatenates two strings
+ *
+ * @s1: string of chars
+ * @s2: string of chars
+ * @n: size of s2 to copy
+ *
+ * Return: address of the newly allocated memory
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *arr;
-	unsigned int i, j, co, co_2;
+	unsigned int len1, len2 = 0;
+	unsigned int i, j;
+	char *str_copy;
+	char *tmp1 = s1;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-	}
+	i = 0;
+	while (*s1++)
+		i++;
+	len1 = i;
+	s1 = tmp1;
 
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-	}
+	len2 = n;
 
-	if (n < j)
-		j = n;
-
-	j += i;
-	arr = malloc(sizeof(char *) * (j + 1));
-
-	if (arr == NULL)
+	str_copy = malloc((len1 + len2) * sizeof(char) + 1);
+	if (str_copy == NULL)
 		return (NULL);
 
-	for (co = 0; co < i; co++)
-		arr[co] = s1[co];
-	for (co_2 = 0; co < j; co_2++)
+	j = 0;
+	while (j < len1)
 	{
-		arr[co] = s2[co_2];
-		co++;
+		str_copy[j] = s1[j];
+		j++;
 	}
-	co++;
-	arr[co] = '\0';
-	return (arr);
+	while (j < len1 + len2)
+	{
+		str_copy[j] = s2[j - len1];
+		j++;
+	}
+	str_copy[j] = '\0';
+	return (str_copy);
 }
