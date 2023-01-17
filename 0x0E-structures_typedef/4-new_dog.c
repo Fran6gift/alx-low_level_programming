908557e830b325f8b22e101ecf238ce8e5827771
Fran6gift <111250094+Fran6gift@users.noreply.github.com>
2023-01-18 00:14:35 +0100

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,81 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "dog.h"
-
+#include <stdlib.h>
 /**
- * new_dog - function that creates a new dog
- * @name: passed from main
- * @age: passed from main
- * @owner: passed from main
- *
- * Return: pointer to the struct
- */
-
+  * new_dog - creates a new struct of type dog
+  * @name: struct parameter name
+  * @age: struct parameter age
+  * @owner: struct parameter owner
+  * Return: returns pointer to buffer of datatype dog
+  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *ptr;
+	int nlen, olen, i;
+	dog_t *doggy;
 
-	if (name == NULL || owner == NULL)
-		return (NULL);
-	ptr = malloc(sizeof(dog_t));
-	if (ptr != NULL)
-	{
-		ptr->name = _strdup(name);
-		if (ptr->name == NULL)
-		{
-			free(ptr->name);
-			free(ptr);
-			return (NULL);
-		}
-		ptr->age = age;
-		ptr->owner = _strdup(owner);
-		if (ptr->owner == NULL)
-		{
-			free(ptr->name);
-			free(ptr->owner);
-			free(ptr);
-			return (NULL);
-		}
-		return (ptr);
-	}
-	return (NULL);
-}
-
-/**
- * _strdup - function that returns a pointer to a newly allocated space
- * in memory, which contains a copy of the string given as a parameter
- *
- * @str: string of chars
- *
- * Return: address of the newly allocated memory
- */
-
-char *_strdup(char *str)
-{
-	unsigned int len;
-	unsigned int i, j;
-	char *str_copy;
-	char *tmp = str;
-
-	if (str == NULL)
+	nlen = olen = 0;
+	while (name[nlen++])
+		;
+	while (owner[olen++])
+		;
+	doggy = malloc(sizeof(dog_t));
+	if (doggy == NULL)
 		return (NULL);
 
-	i = 0;
-	while (*str++)
-		i++;
-	len = i;
-	str = tmp;
-
-	str_copy = malloc(len * sizeof(char) + 1);
-	if (str_copy == NULL)
+	doggy->name = malloc(nlen * sizeof(doggy->name));
+	if (doggy == NULL)
 		return (NULL);
+	for (i = 0; i < nlen; i++)
+		doggy->name[i] = name[i];
 
-	j = 0;
-	while (j < len)
-	{
-		str_copy[j] = str[j];
-		j++;
-	}
-	str_copy[j] = '\0';
-	return (str_copy);
+	doggy->age = age;
+
+	doggy->owner = malloc(olen * sizeof(doggy->owner));
+	if (doggy == NULL)
+		return (NULL);
+	for (i = 0; i < olen; i++)
+		doggy->owner[i] = owner[i];
+	return (doggy);
 }
