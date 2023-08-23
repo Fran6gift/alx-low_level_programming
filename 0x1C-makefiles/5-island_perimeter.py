e9e6fb1ce75a322bb1f9656ab42a4f7f80ed9a3d
Fran6gift <111250094+Fran6gift@users.noreply.github.com>
2023-08-23 17:41:59 +0100

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
-"""Defines an island perimeter measuring function."""
+"""
+Island Perimeter
+"""
+
 
 def island_perimeter(grid):
-    """Return the perimiter of an island.
-
-    The grid represents water by 0 and land by 1.
-
-    Args:
-        grid (list): A list of list of integers representing an island.
-    Returns:
-        The perimeter of the island defined in grid.
-    """
-    width = len(grid[0])
-    height = len(grid)
-    edges = 0
-    size = 0
-
-    for i in range(height):
-        for j in range(width):
+    """function that returns the perimeter of the island described in grid"""
+    count = 0
+    for i in range(0, len(grid)):
+        for j in range(0, len(grid[i])):
             if grid[i][j] == 1:
-                size += 1
-                if (j > 0 and grid[i][j - 1] == 1):
-                    edges += 1
-                if (i > 0 and grid[i - 1][j] == 1):
-                    edges += 1
-    return size * 4 - edges * 2
+                count += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    count -= 1
+                if i < len(grid) - 1 and grid[i + 1][j] == 1:
+                    count -= 1
+                if j > 0 and grid[i][j - 1] == 1:
+                    count -= 1
+                if j < len(grid[i]) - 1 and grid[i][j + 1] == 1:
+                    count -= 1
+    return count
